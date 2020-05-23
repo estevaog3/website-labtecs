800411c6b186cee1b707f1ce1a87613cab7fa3e7
EstevaoCostaG3 <estevao_carvalho.10@hotmail.com>
2020-05-23 13:29:43 -0400

    Adiciona NumbersForm e Numbers ao App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Number from "./components/Number";
+import NumbersForm from "./components/NumbersForm";
 
 function App() {
   return (
     <div className="App">
+      <main>
+        <h1 className="heading">Insira 5 números</h1>
+        <NumbersForm>
+          <Number label="1º número" min={1} max={10} />
+          <Number label="2º número" min={1} max={10} />
+          <Number label="3º número" min={1} max={10} />
+          <Number label="4º número" min={1} max={10} />
+          <Number label="5º número" min={1} max={10} />
+        </NumbersForm>
+      </main>
     </div>
   );
 }
