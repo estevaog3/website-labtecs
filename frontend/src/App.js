1977fc8ba54375022017e8357e008238d7cdafae
EstevaoCostaG3 <estevao_carvalho.10@hotmail.com>
2020-05-23 21:15:55 -0400

    Adiciona estado à 'App'
    O estado é necessário para dá feedback de acordo com o resposta à requisição realizada.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Number from "./components/Number";
 import NumbersForm from "./components/NumbersForm";
+import Spin from "./components/Spin";
+import Message from "./components/Message";
 
 function App() {
+  const [isFormSubmited, setIsFormSubmited] = useState(false);
+  const [isFormPending, setIsFormPending] = useState(false);
+  const [isFormSubmitedWithSuccess, setIsFormSubmitedWithSuccess] = useState(
+    undefined
+  );
+
+  const successMessage = "Tudo certo";
+  const errorMessage = "Houve um erro durante a operação";
+
+  const beginFormSubmition = () => {
+    setIsFormSubmited(false);
+    setIsFormPending(true);
+  };
+
+  const endFormSubmition = (isSuccess) => {
+    setIsFormPending(false);
+    setIsFormSubmited(true);
+    setIsFormSubmitedWithSuccess(isSuccess);
+  };
+
+  const provideFeedback = () => {
+    if (!isFormSubmited && !isFormPending) {
+      return null;
+    }
+    if (isFormSubmited) {
+      return isFormSubmitedWithSuccess ? (
+        <Message isSuccess={isFormSubmitedWithSuccess} value={successMessage} />
+      ) : (
+        <Message isSuccess={isFormSubmitedWithSuccess} value={errorMessage} />
+      );
+    }
+    return <Spin />;
+  };
   return (
     <div className="App">
       <main>
         <h1 className="heading">Insira 5 números</h1>
-        <NumbersForm>
+        <NumbersForm
+          beginFormSubmition={beginFormSubmition}
+          endFormSubmition={endFormSubmition}
+        >
           <Number label="1º número" min={1} max={10} />
           <Number label="2º número" min={1} max={10} />
           <Number label="3º número" min={1} max={10} />
           <Number label="4º número" min={1} max={10} />
           <Number label="5º número" min={1} max={10} />
         </NumbersForm>
+        {provideFeedback()}
       </main>
     </div>
   );
