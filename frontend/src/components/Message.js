70e04448e24a48bdfdaf92550c0e096b77ea3b36
EstevaoCostaG3 <estevao_carvalho.10@hotmail.com>
2020-05-24 10:16:09 -0400

    Atualiza estilo em 'Message': texto com cor preta quando há sucesso

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -3,12 +3,8 @@ import "./Message.css";
 
 export default function ({ value, isSuccess }) {
   let classNames = "message";
-  if (isSuccess !== undefined) {
-    if (isSuccess) {
-      classNames = classNames + " success";
-    } else {
-      classNames = classNames + " error";
-    }
+  if (!isSuccess) {
+    classNames = classNames + " error";
   }
   return <h4 className={classNames}>{value}</h4>;
 }
